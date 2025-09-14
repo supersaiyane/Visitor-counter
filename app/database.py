94715f4f71b1c11689eb7231d118d0ed154e266f
Gurpreet Singh <49033319+supersaiyane@users.noreply.github.com>
2025-09-14 07:38:21 +0530

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./visitors.db"
+# Use disk-mount path from Render
+SQLALCHEMY_DATABASE_URL = "sqlite:////data/visitors.db"
+
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
